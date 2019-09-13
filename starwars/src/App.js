24f7ebb0eb6805049a76f2bfe52ff3418e8a9b0d
Ayomide Jones <ayormeday@gmail.com>
2019-09-13 09:04:52 +0100

    downloaded axios

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from "axios";
 import './App.css';
 
+
+
+const starWarsApi = "https://lambda-github-api-server.herokuapp.com/"
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
-
+  // the state properties here
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  const [character, setCharacter] = useState({});
+
+  useEffect(() => {
+    axios
+    .get(starWarsApi)
+    .then(response => {
+      setCharacter(response.data);
+    })
+    .catch(error => console.log("invalid Api"))
+  })
 
   return (
     <div className="App">
